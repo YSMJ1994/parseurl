f408bb00f73f01580bd2295dd8ee87ad1d385733
SoberZ <710411486@qq.com>
2018-04-18 00:04:41 +0800

    first commit

---
+++ src/main/java/com/cloud/parseurl/ParseUrlController.java
@@ -0,0 +1,43 @@
+package com.cloud.parseurl;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 创建自: Sober 时间: 2018/4/17.
+ */
+@RestController
+@RequestMapping(value = "/")
+public class ParseUrlController {
+
+    @RequestMapping(value = "/parse", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String parseUrl(@RequestParam Map<String, String> map) {
+        return resolveMap(map);
+    }
+
+    String resolveMap(Map<String, String> map) {
+        Map<String, String> result = new HashMap<>();
+        for (String key : map.keySet()) {
+            result.put(encodeing(key), encodeing(map.get(key)));
+        }
+
+        return "111";
+    }
+
+    String encodeing(String str) {
+        try {
+            String firstEncode = URLEncoder.encode(str, "UTF-8");
+            firstEncode = firstEncode.replace("+", "%20");
+            firstEncode = firstEncode.replace("*", "%2A");
+            firstEncode = firstEncode.replace("~", "%7E");
+            return firstEncode;
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+}
---
+++ src/main/java/com/cloud/parseurl/ParseurlApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.parseurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseurlApplication.class, args);
+	}
+}
